432abc553f92a2ae59df530ce596dc197680a0c3
Anubhab Guha <96384072+anubhabguha1999@users.noreply.github.com>
2023-03-12 16:09:02 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,6 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{75bc3107128d061968f4e51cc8cd8f46}",
+  //use your api key here
+  key: "75bc3107128d061968f4e51cc8cd8f46",
   base: "https://api.openweathermap.org/data/2.5/",
 };
